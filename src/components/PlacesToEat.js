77665f6e1a099c5d1f84e0706e527ced4db650d9
SommerCh <47107441+SommerCh@users.noreply.github.com>
2022-04-29 09:20:16 +0200

    Update PlacesToEat.js

--- src/components/PlacesToEat.js
+++ src/components/PlacesToEat.js
@@ -1,11 +1,25 @@
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 import AllPlaces from "./AllPlaces";
+import imagePlaceholder from "../assets/imagePlaceholder.png";
 import Cafes from "./Cafes";
 import Clubs from "./Clubs";
 import Restaurants from "./Restaurants";
+import Search from "./Search";
 
 export default function PlacesToEat() {
   const [change, setChange] = useState();
+  const [places, setPlaces] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("data/placesToEat.json");
+      const data = await response.json();
+      setPlaces(data);
+      console.log(data);
+    }
+    getData();
+  }, []);
 
   function test(e) {
     setChange(e.target.value);
@@ -32,23 +46,38 @@ export default function PlacesToEat() {
       all.classList.add("no");
     } else if (e.target.value === "All") {
       all.classList.remove("no");
+      club.classList.remove("yes");
+      cafe.classList.remove("yes");
     }
   }
 
   return (
     <section className="page">
       <h1>Places To Eat</h1>
+      <input
+        type="text"
+        onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        onkeyup="search(this.value)"
+      />
 
-      <select value={change} onChange={test}>
+<select value={change} onChange={test}>
         <option value="All">All</option>
         <option value="Cafés">Cafés</option>
         <option value="Nightlife and Club">Nightlife and Club</option>
         <option value="Restaurants">Restaurants</option>
       </select>
 
-      <div id="all">
-        <AllPlaces />
-      </div>
+      <section id="all" className="grid-container">
+        {places
+          .filter((place) => place.Name.toLowerCase().includes(search))
+          .map((place) => (
+            <article key={place.Id}>
+              <img src={place.Files[0] ? place.Files[0].Uri : imagePlaceholder} alt={place.Name} />
+              <h2>{place.Name}</h2>
+              <p>{place.Category.Name}</p>
+            </article>
+          ))}
+      </section>
 
       <div className="container">
         <div id="cafe" className="no">
